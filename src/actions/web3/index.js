03a6c186e950dd788ffd8e756436b68a00c4dc35
luzzif <fedeluzzi00@gmail.com>
2020-06-30 19:03:32 +0200

    fix: silently fail on web3 aborted web3 initialization

--- src/actions/web3/index.js
+++ src/actions/web3/index.js
@@ -1,9 +1,6 @@
 import { getWeb3Modal } from "../../containers/app";
 import { postLogout } from "../loopring";
-import React from "react";
 import Web3 from "web3";
-import { toast } from "react-toastify";
-import { FormattedMessage } from "react-intl";
 
 export const INITIALIZE_WEB3_SUCCESS = "INITIALIZE_WEB3_SUCCESS";
 
@@ -20,7 +17,6 @@ export const initializeWeb3 = () => async (dispatch) => {
         });
         dispatch({ type: INITIALIZE_WEB3_SUCCESS, web3: new Web3(provider) });
     } catch (error) {
-        toast.error(<FormattedMessage id="error.web3.initialization" />);
         console.error("error initializing web3", error);
     }
 };
