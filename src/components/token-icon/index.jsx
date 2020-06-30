92e344302a2e9a7b2f8d976db1b9c03ab973fff0
luzzif <fedeluzzi00@gmail.com>
2020-06-30 22:25:12 +0200

    feat: add nest token icon

--- src/components/token-icon/index.jsx
+++ src/components/token-icon/index.jsx
@@ -2,24 +2,30 @@ import React from "react";
 import PropTypes from "prop-types";
 import { StyledImage } from "./styled";
 import keepLogo from "../../images/keep.png";
+import nestLogo from "../../images/nest.png";
 
-// If the address is ETH, we simply fallback to the WETH icon.
-// For KEEP, we use a custom icon
-export const TokenIcon = ({ address, size }) => (
-    <StyledImage
-        alt="Icon"
-        size={size}
-        src={
-            address === "0x85Eee30c52B0b379b046Fb0F85F4f3Dc3009aFEC"
-                ? keepLogo
-                : `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${
-                      address === "0x0000000000000000000000000000000000000000"
-                          ? "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
-                          : address
-                  }/logo.png`
+export const TokenIcon = ({ address, size }) => {
+    const getIconSource = () => {
+        switch (address) {
+            case "0x85Eee30c52B0b379b046Fb0F85F4f3Dc3009aFEC": {
+                return keepLogo;
+            }
+            case "0x04abEdA201850aC0124161F037Efd70c74ddC74C": {
+                return nestLogo;
+            }
+            default: {
+                // If the address is ETH, we simply fallback to the WETH icon.
+                return `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${
+                    address === "0x0000000000000000000000000000000000000000"
+                        ? "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+                        : address
+                }/logo.png`;
+            }
         }
-    />
-);
+    };
+
+    return <StyledImage alt="Icon" size={size} src={getIconSource()} />;
+};
 
 TokenIcon.propTypes = {
     address: PropTypes.string.isRequired,
